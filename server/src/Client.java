42f1b62c1393170ada75401ea3f9952cdc6ca3b1
Victor Freixinho <victorfn01@gmail.com>
2021-11-10 13:03:06 -0300

    Add login message handling and json-simple lib

--- server/src/Client.java
+++ server/src/Client.java
@@ -1,18 +1,29 @@
 import java.net.Socket;
 
 public class Client {
+
+    private static int count = 0;
+    private int id;
     private Socket clientSocket;
     private String clientName;
+    private boolean isPlaying;
+    private int opponentID;
 
     private Client(){}
 
     Client(Socket clientSocket) {
         this.clientSocket = clientSocket;
         this.clientName = "";
+        this.isPlaying = false;
+        this.opponentID = -1;
+        this.id = ++count;
     }
     Client(Socket clientSocket, String clientName) {
         this.clientSocket = clientSocket;
         this.clientName = clientName;
+        this.isPlaying = false;
+        this.opponentID = -1;
+        this.id = ++count;
     }
 
     public Socket getClientSocket() {
@@ -23,7 +34,31 @@ public class Client {
         return clientName;
     }
 
+    public boolean isPlaying() {
+        return isPlaying;
+    }
+
+    public int getOpponentID() {
+        return opponentID;
+    }
+
+    public int getId() {
+        return id;
+    }
+
     public void setClientName(String clientName) {
         this.clientName = clientName;
+    }
+
+    public void setState(boolean isPlaying) {
+        if(!isPlaying) {
+            opponentID = -1;
+        }
+        this.isPlaying = isPlaying;
+
+    }
+
+    public void setOpponentID(int opponentID) {
+        this.opponentID = opponentID;
     }
 }
--- server/src/Server.java
+++ server/src/Server.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -6,6 +7,9 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Server {
 
@@ -71,8 +75,70 @@ public class Server {
         }
     }
 
+    private Client searchForOpponent(Client client) {
+        for(Client c : clientsList) {
+            if(c.getId() != client.getId()) {
+                if (c.getClientSocket().isClosed() || !c.getClientSocket().isConnected()) {
+                    //TODO: Confirm if this is safe
+                    clientsList.remove(c);
+                    System.out.println("Removing client from list");
+                } else if (!c.isPlaying()) {
+                    return c;
+                }
+            }
+        }
+        return null;
+    }
+
+    private void startGameWithPlayers(Client player1, Client player2) {
+        player1.setOpponentID(player2.getId());
+        player1.setState(true);
+        player2.setOpponentID(player1.getId());
+        player2.setState(true);
+        boolean isFirstPlayer = Math.random() > 0.5;
+        sendGameConfig(player1, player2.getClientName(), isFirstPlayer);
+        sendGameConfig(player2, player1.getClientName(), !isFirstPlayer);
+    }
+
+    private void sendGameConfig(Client client, String opponentName, boolean isFirstPlayer) {
+        try {
+            PrintWriter clientWriter = new PrintWriter(client.getClientSocket().getOutputStream());
+            JSONObject obj = new JSONObject();
+            obj.put("OpponentName", opponentName);
+            obj.put("isFirstPlayer", isFirstPlayer);
+            clientWriter.println("/login");
+            clientWriter.println(obj.toJSONString());
+            System.out.println("Sending initial configs to client...");
+            clientWriter.flush();
+        } catch(IOException e) {
+            System.out.println("Error sending Message to client:");
+            e.printStackTrace();
+        } catch(Exception e) {
+            System.out.println("Unexpected error sending Message to client:");
+            e.printStackTrace();
+        }
+    }
+
     private void handleClientMessage(Client client, String header, String message) {
-        System.out.println("Header: " + header);
-        System.out.println("Message: " + message);
+        switch(header) {
+            case "/login":
+                try {
+                    JSONParser parser = new JSONParser();
+                    JSONObject obj = (JSONObject) parser.parse(message);
+                    System.out.println("Received client name: " + obj.get("name"));
+                    client.setClientName((String) obj.get("name"));
+                } catch (ParseException e) {
+                    System.out.println("Error Parsing JSON object, to get client name.");
+                    e.printStackTrace();
+                }
+                Client opponent = searchForOpponent(client);
+                if(opponent != null) {
+                    startGameWithPlayers(client, opponent);
+                }
+                break;
+            default:
+                System.out.println("Incorrect Client Message header.");
+                break;
+        }
     }
 }
