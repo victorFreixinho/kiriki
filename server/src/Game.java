349f2c55e82de1b8638c058156585c91ad4b656b
Victor Freixinho <victorfn01@gmail.com>
2021-11-11 03:13:41 -0300

    Add game logic

--- server/src/Game.java
+++ server/src/Game.java
@@ -61,25 +61,15 @@ public class Game {
     }
 
     private void sendGameConfigTo(Client player, String header, boolean isFirstPlayer) {
-        try {
-            PrintWriter clientWriter = new PrintWriter(player.getClientSocket().getOutputStream());
-            JSONObject jsonObj = new JSONObject();
-            boolean isPlayer1 = player.equals(player1);
-            jsonObj.put("opponentName", isPlayer1 ? player2.getClientName() : player1.getClientName());
-            jsonObj.put("opponentSum", isPlayer1 ? getDicesSum(dices2) : getDicesSum(dices1));
-            jsonObj.put("isFirstPlayer", isFirstPlayer);
-            jsonObj.put("initialDices", isPlayer1 ? dices1 : dices2);
-            clientWriter.println(header);
-            clientWriter.println(jsonObj.toJSONString());
-            System.out.println("Sending initial configs to client...");
-            clientWriter.flush();
-        } catch(IOException e) {
-            System.out.printf("Error sending game initial config to client %s:\n", player.getClientName());
-            e.printStackTrace();
-        } catch(Exception e) {
-            System.out.printf("Unexpected error sending game initial config to client %s:", player.getClientName());
-            e.printStackTrace();
-        }
+        boolean isPlayer1 = player.equals(player1);
+
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("opponentName", isPlayer1 ? player2.getClientName() : player1.getClientName());
+        jsonObj.put("opponentSum", isPlayer1 ? getDicesSum(dices2) : getDicesSum(dices1));
+        jsonObj.put("isFirstPlayer", isFirstPlayer);
+        jsonObj.put("initialDices", isPlayer1 ? dices1 : dices2);
+
+        sendMessage(player, Server.HEADER_GAME_CONFIG, jsonObj.toJSONString(), "initial configs message");
     }
 
     private List<Integer> sortInitialDices() {
@@ -96,5 +86,94 @@ public class Game {
             sum += d;
         }
         return sum;
+    }
+
+    public void handlePlayerGuess(Client player, ArrayList<Integer> guess) {
+        boolean isPlayer1 = player.getId() == player1.getId();
+        ArrayList<Integer> opponentDicesCopy = (ArrayList<Integer>) (isPlayer1 ? dices2 : dices1);
+        int points = 0;
+        for(int i : guess) {
+            if(opponentDicesCopy.contains(i)) {
+                opponentDicesCopy.remove(i);
+                points++;
+            }
+        }
+
+        if(guess.size() == points) {
+            sendFinishGameMessage(isPlayer1);
+        } else {
+            if(isPlayer1 ? dices1.size()>2 : dices2.size()>2) {
+                if(guess.size() < 2 * points) {
+                    sendWrongGuessMessage(isPlayer1, false);
+                    sendStartRoundMessage(!isPlayer1, false);
+                } else {
+                    sendWrongGuessMessage(isPlayer1, true);
+                    sendStartRoundMessage(!isPlayer1, true);
+                }
+            } else {
+                sendFinishGameMessage(!isPlayer1);
+            }
+        }
+    }
+
+    private void sendWrongGuessMessage(boolean isPlayer1, boolean loseDice) {
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("loseDice", loseDice);
+
+        sendMessage(
+                isPlayer1 ? player1 : player2,
+                Server.HEADER_PLAYER_GUESS,
+                jsonObj.toJSONString(),
+            "wrong guess message"
+        );
+    }
+
+    private void sendStartRoundMessage(boolean isPlayer1, boolean loseDice) {
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("loseDice", loseDice);
+
+        sendMessage(
+                isPlayer1 ? player1 : player2,
+                Server.HEADER_START_ROUND,
+                jsonObj.toJSONString(),
+                "start round message"
+        );
+    }
+
+    private void sendFinishGameMessage(boolean isPlayer1Winner) {
+        JSONObject winnerJson = new JSONObject();
+        JSONObject loserJson = new JSONObject();
+        winnerJson.put("winner", true);
+        loserJson.put("winner", false);
+
+        sendMessage(
+                player1,
+                Server.HEADER_FINISH_GAME,
+                isPlayer1Winner ? winnerJson.toJSONString() : loserJson.toJSONString(),
+                "finish game message"
+        );
+
+        sendMessage(
+                player2,
+                Server.HEADER_FINISH_GAME,
+                !isPlayer1Winner ? winnerJson.toJSONString() : loserJson.toJSONString(),
+                "finish game message"
+        );
+    }
+
+    private void sendMessage(Client player, String header, String message, String messageDescription) {
+        try {
+            PrintWriter clientWriter = new PrintWriter(player.getClientSocket().getOutputStream());
+            clientWriter.println(header);
+            clientWriter.println(message);
+            System.out.println("Sending " + messageDescription + " to client...");
+            clientWriter.flush();
+        } catch(IOException ex) {
+            System.out.printf("Error sending %s to client %s:\n", messageDescription, player.getClientName());
+            ex.printStackTrace();
+        } catch(Exception ex) {
+            System.out.printf("Unexpected error sending %s to client %s:", messageDescription, player.getClientName());
+            ex.printStackTrace();
+        }
     }
 }
